8a426051af35b9733d0417cbd8949fbc4e63a60a
Gabe Bezerra <gabe.bezerra.ext@loccitane.com>
2023-12-02 02:03:45 -0300

    Adding basic styles

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ function App() {
   const [cats, setCats] = useState([])
   const [filteredCats, setFilteredCats] = useState([])
   const [filterMonth, setFilterMonth] = useState('Todos')
+  const [isSuccess, setIsSuccess] = useState(false)
 
   const registerForm = (e) => {
     e.preventDefault();
     const newCats = cats.concat({
       mes: e.target.mes.value,
       recebidos: e.target.recebidos.value,
-      atendidos: e.target.atendidos.value
+      atendidos: e.target.atendidos.value,
+      id: cats.length
     })
     setCats(newCats)
 
+    setIsSuccess(true)
+
+    setTimeout(() => {
+      setIsSuccess(false)
+    }, 4000)
   }
 
   const handleFilter = (e) => {
@@ -42,16 +49,17 @@ function App() {
     <div className="App">
       {!hasStarted &&
         <div className="initRegister">
+            <img alt="Corpo socorrido na rua" src="https://www.lawdonut.co.uk/business/sites/lawdonut-business/files/dealingwithanaccident_157411556.jpg"></img>
             <h1>
-                Bem vindo ao registrador de incidentes!
-                <button onClick={() => setHasStarted(true)}>Iniciar</button>
+                Bem vindo ao registrador de acidentes!
             </h1>
+            <button className='mainButton' onClick={() => setHasStarted(true)}>Iniciar</button>
         </div>  
       }
       
       {hasStarted &&
-          <div>
-
+          <div className='loggedApp'>
+            <h1>Gerenciador de Ocorrências</h1>
             <div className='tabMenu'>
                 <button className={`${currentTab === 'register' ? 'tab tab--active' : 'tab'}`} onClick={() => setCurrentTab("register")}>Cadastrar Ocorrência</button>
                 <button className={`${currentTab === 'read' ? 'tab tab--active' : 'tab'}`} onClick={() => setCurrentTab("read")}>Ver Ocorrências</button>
@@ -62,24 +70,34 @@ function App() {
                 <div className='tabContent'>
                   <h2>Cadastre uma nova ocorrência</h2>
                   <form onSubmit={registerForm}>
-                      <select value={filterMonth} name="mes">
-                          <option>Janeiro</option>
-                          <option>Fevereiro</option>
-                          <option>Março</option>
-                          <option>Abril</option>
-                          <option>Maio</option>
-                          <option>Junho</option>
-                          <option>Julho</option>
-                          <option>Agosto</option>
-                          <option>Setembro</option>
-                          <option>Outubro</option>
-                          <option>Novembro</option>
-                          <option>Dezembro</option>
-                      </select>
-                      <input name="recebidos" type="number" placeholder='Número de Chamados Recebidos'></input>
-                      <input name="atendidos" type="number" placeholder='Número de Chamados Atendidos'></input>
-                      <button>Registrar</button>
+                      <label for="mes">
+                          Mês
+                          <select value={filterMonth} name="mes">
+                              <option>Janeiro</option>
+                              <option>Fevereiro</option>
+                              <option>Março</option>
+                              <option>Abril</option>
+                              <option>Maio</option>
+                              <option>Junho</option>
+                              <option>Julho</option>
+                              <option>Agosto</option>
+                              <option>Setembro</option>
+                              <option>Outubro</option>
+                              <option>Novembro</option>
+                              <option>Dezembro</option>
+                          </select>
+                      </label>
+                      <label for="recebidos">
+                        Número de Chamados Recebidos
+                        <input name="recebidos" type="number" placeholder='0'></input>
+                      </label>
+                      <label for="atendidos">
+                        Número de Chamados Atendidos
+                        <input name="atendidos" type="number" placeholder='0'></input>
+                      </label>
+                      <button className='mainButton'>Registrar</button>
                   </form>
+                  {isSuccess && <p className='successMessage'>Acidente registrado com sucesso</p>}
                 </div>
                 : 
 
@@ -103,7 +121,7 @@ function App() {
 
                     <div className='filteredCats'>
                       {filteredCats.map(cat => 
-                          <div>
+                          <div className='catLine'>
                             <p>Mês: {cat.mes}</p>
                             <p>Recebidos: {cat.recebidos}</p>
                             <p>Atendidos: {cat.atendidos}</p>
