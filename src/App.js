7cfc3d6c9cf13e5763d4b9c51ff90db953dbeb0a
Gabe Bezerra <gabe.bezerra.ext@loccitane.com>
2023-12-02 01:21:40 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  
+  const [hasStarted, setHasStarted] = useState(false)
+  const [currentTab, setCurrentTab] = useState("register")
+  const [cats, setCats] = useState([])
+  const [filteredCats, setFilteredCats] = useState([])
+  const [filterMonth, setFilterMonth] = useState('Todos')
+
+  const registerForm = (e) => {
+    e.preventDefault();
+    const newCats = cats.concat({
+      mes: e.target.mes.value,
+      recebidos: e.target.recebidos.value,
+      atendidos: e.target.atendidos.value
+    })
+    setCats(newCats)
+
+  }
+
+  const handleFilter = (e) => {
+    e.preventDefault();
+    const selectedMonth = e.target.value;
+    setFilterMonth(selectedMonth);
+    if(selectedMonth === 'Todos') {
+      setFilteredCats(cats)
+    } else {
+      setFilteredCats(cats.filter(cat => cat.mes === selectedMonth))
+    }
+  }
+  
+
+  useEffect(() => {
+    if(currentTab === 'read') {
+      setFilteredCats(cats)
+    }
+  }, [currentTab, cats])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!hasStarted &&
+        <div className="initRegister">
+            <h1>
+                Bem vindo ao registrador de incidentes!
+                <button onClick={() => setHasStarted(true)}>Iniciar</button>
+            </h1>
+        </div>  
+      }
+      
+      {hasStarted &&
+          <div>
+
+            <div className='tabMenu'>
+                <button className={`${currentTab === 'register' ? 'tab tab--active' : 'tab'}`} onClick={() => setCurrentTab("register")}>Cadastrar Ocorrência</button>
+                <button className={`${currentTab === 'read' ? 'tab tab--active' : 'tab'}`} onClick={() => setCurrentTab("read")}>Ver Ocorrências</button>
+                <button className="logout" onClick={() => setHasStarted(false)}>Sair</button>
+            </div>
+            <div className='tabContainer'>
+              {currentTab === 'register' ? 
+                <div className='tabContent'>
+                  <h2>Cadastre uma nova ocorrência</h2>
+                  <form onSubmit={registerForm}>
+                      <select value={filterMonth} name="mes">
+                          <option>Janeiro</option>
+                          <option>Fevereiro</option>
+                          <option>Março</option>
+                          <option>Abril</option>
+                          <option>Maio</option>
+                          <option>Junho</option>
+                          <option>Julho</option>
+                          <option>Agosto</option>
+                          <option>Setembro</option>
+                          <option>Outubro</option>
+                          <option>Novembro</option>
+                          <option>Dezembro</option>
+                      </select>
+                      <input name="recebidos" type="number" placeholder='Número de Chamados Recebidos'></input>
+                      <input name="atendidos" type="number" placeholder='Número de Chamados Atendidos'></input>
+                      <button>Registrar</button>
+                  </form>
+                </div>
+                : 
+
+                <div className='tabContent'>
+                  <h2>Visualizar Chamados</h2>
+                  <select onChange={(e) => {handleFilter(e)}} name="mes">
+                        <option>Todos</option>
+                        <option>Janeiro</option>
+                        <option>Fevereiro</option>
+                        <option>Março</option>
+                        <option>Abril</option>
+                        <option>Maio</option>
+                        <option>Junho</option>
+                        <option>Julho</option>
+                        <option>Agosto</option>
+                        <option>Setembro</option>
+                        <option>Outubro</option>
+                        <option>Novembro</option>
+                        <option>Dezembro</option>
+                    </select>
+
+                    <div className='filteredCats'>
+                      {filteredCats.map(cat => 
+                          <div>
+                            <p>Mês: {cat.mes}</p>
+                            <p>Recebidos: {cat.recebidos}</p>
+                            <p>Atendidos: {cat.atendidos}</p>
+                          </div>    
+                      )}
+                    </div>
+                </div>
+              }
+            </div>
+
+          </div>  
+      }
+
     </div>
   );
 }
